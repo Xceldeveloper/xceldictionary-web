1b0d213c25dc7741a119ef908391b0051573c932
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2021-02-11 06:13:04 +0100

    small update

--- pages/search.vue
+++ pages/search.vue
@@ -1,39 +1,41 @@
 <template>
   <div id="wrapperx">
-    <v-toolbar
-      width="100%"
-      height="60px"
-      dark
-      color="#000"
-      elevation="1"
-      dense
-      floating
-    >
+    <v-app-bar width="100%" height="60px" dark color="#000" elevation="1" dense>
       <v-btn icon @click="$router.go(-1)">
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
-      <v-text-field
-        hide-details
-        elevation="0"
-        v-model="searchQuery"
-        prepend-inner-icon="mdi-magnify"
-        single-line
-        solo
-      ></v-text-field>
+      <v-spacer></v-spacer>
 
-      <v-btn icon>
-        <v-icon>mdi-microphone</v-icon>
-      </v-btn>
-    </v-toolbar>
+      <div id="sexc">
+        <v-text-field
+          hide-details
+          autofocus
+          elevation="0"
+          width="400px"
+          v-model="searchQuery"
+          prepend-inner-icon="mdi-magnify"
+          :append-icon="searchQuery == '' ? 'mdi-microphone' : null"
+          @click:append="showMic = true"
+          clearable
+          single-line
+          solo
+          :rounded="$vuetify.breakpoint.xs ? false : true"
+        ></v-text-field>
+      </div>
+
+     
+     <v-spacer></v-spacer>
+
+    </v-app-bar>
     <v-progress-linear
-      v-if="loading && searchQuery.length > 0"
+      v-if="loading && searchQuery != '' && searchQuery != null"
       height="2px"
       color="#000"
       indeterminate
       width="100%"
     />
-    <div class="search-result-cover">
-      <v-list v-if="searchQuery.length == 0 && histories.length > 0">
+    <div class="search-result-cover" v-if="histories.length > 0 || autoCompleted.length > 0">
+      <v-list v-if="searchQuery == '' || searchQuery == null  && histories.length > 0">
         <v-list-item
           link
           v-for="(history, index) in histories"
@@ -49,7 +51,7 @@
         </v-list-item>
       </v-list>
 
-      <v-list v-if="searchQuery.length > 0 && autoCompleted.length > 0">
+      <v-list v-if="searchQuery != '' || searchQuery != null  && autoCompleted.length > 0">
         <v-list-item
           link
           v-for="(autoComplete, index) in autoCompleted"
@@ -60,11 +62,31 @@
             <v-icon>mdi-text</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            {{ autoComplete.word }}
+            <span
+              :style="{
+                'font-weight': autoComplete.word == searchQuery ? 'bold' : null,
+              }"
+              >{{ autoComplete.word }}</span
+            >
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </div>
+
+
+    <v-bottom-sheet max-width="400px" v-model="showMic">
+      <v-card>
+        <v-card-text align="center" justify="center">
+          <br>
+          <v-btn color="#000" fab height="80px" width="80px">
+            <v-icon color="#fff" size="50px">mdi-microphone</v-icon>
+          </v-btn>
+          <br>
+           <br>
+          <span>Pronouce Word</span>
+        </v-card-text>
+      </v-card>
+    </v-bottom-sheet>
   </div>
 </template>
 
@@ -79,6 +101,7 @@ export default {
       searchQuery: "",
       autoCompleted: [],
       loading: false,
+      showMic:false
     };
   },
 
@@ -93,11 +116,12 @@ export default {
           s: val,
         })
         .then((json) => {
-          console.log(json);
+        
           this.autoCompleted = json;
           this.loading = false;
           //do it!
         });
+
     },
   },
   methods: {
@@ -165,9 +189,18 @@ export default {
 
 .search-result-cover {
   width: 100vw;
-  height: calc(100vh - 70px);
-  max-width: 600px;
-  margin: 0px auto;
+  height: auto;
+  max-height: calc(100vh - 70px);
+  max-width: 750px;
+  margin: 5px auto;
   overflow: auto;
+  box-shadow: 0px  1px 10px lightgrey;
+  border-radius: 10px;
+}
+
+#sexc {
+  width: calc(100vw - 45px);
+  max-width: 700px;
+  margin: auto;
 }
 </style>
